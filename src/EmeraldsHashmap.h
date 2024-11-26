4a640884737a8d8794d537fa92192bf7455765e3
oblivious <thanos.papap42@gmail.com>
2024-11-26 22:27:52 +0200

    Updated license with special provision.

--- src/EmeraldsHashmap.h
+++ src/EmeraldsHashmap.h
@@ -2,7 +2,7 @@
  * A hashmap implementation using the Robert Jenkins' 32 bit mix function with
  * Knuth's multiplicative method for hashing.
  *
- * Copyright (C) 2024 oblivious
+ * Copyright (C) 2021-2025 oblivious
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,6 +16,14 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * As a special exception, this library may be used in programs licensed
+ * under any terms.  Modifications to the library itself must be licensed
+ * under the GNU General Public License version 3, with the inclusion of
+ * this special exception, while modifications to programs using this
+ * library may continue to be licensed under any terms.  This exception
+ * does not impose any additional licensing requirements, modify or
+ * transform the licensing terms of programs using this library.
  */
 
 #ifndef __EMERALDSHASHMAP_H_
